673ca4974a74690e5274dd4f65273ae85b4abf50
Thomas Clifton <tclifton@umail.ucsb.edu>
2018-12-06 19:05:39 -0800

    update main

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,86 @@
 #include <string>
 #include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <vector>
+
+#include "User.h"
 
 using namespace std;
 
+struct User{
+  int perm;
+  string name;
+  string genre1;
+  string genre2;
+}
+
+struct Entry{
+  int index;
+  User user;
+}
+
+struct Node{
+  Entry *data;
+  Node **children;
+  bool leaf;
+  int n;
+  int degree;
+};
+
+Node *root = NULL, *np = NULL, *x = NULL;
+
+Node * createNewNode(){
+  tempNode = new Node;
+  tempNode->data = new Entry[3];
+  tempNode->children = new Node *[4];
+  tempNode->isLeaf = true;
+  tempNode->degree = 0;
+  for(int i=0;i<4;i++){
+    tempNode->children[i] = NULL;
+  }
+  return tempNode;
+}
+
+void insertion(string fullstr){
+  User user;
+  int index,numFriends,count=4;
+  string temp;
+  vector<string> vec;
+  vector<int> numvec;
+  stringstream ss(fullstr);
+
+  while(ss.good()){
+    string substr;
+    getline(ss,substr,';');
+    vec.push_back(substr);
+  }
+
+  user.perm=atoi(vec[0]);
+  user.name=vec[1];
+  user.genre1=vec[2];
+  user.genre2=vec[3];
+
+  index = graph.size();
+  while(count < vec.size()){
+    numvec.push_back(atoi(vec[count]));
+  }
+  graph.push_back(numvec);
+  insertBTree(user.perm, user, index);
+}
+
+void insertBTree(int perm, User user, int index){
+  
+}
+
 int main(){
+  vector<vector<int> > graph;
   bool q = false;
   int input;
+  string *parsestr;
+  string name;
+
+  BTree tree;
 
   cout << "Welcome!\n";
   while(!q){
@@ -14,9 +89,12 @@ int main(){
       switch (input){
       case 1:
 	//input from file
+	cout << "Please enter filname: ";
+	cin >> name;
 	break;
       case 2:
 	//add user
+	
 	break;
       case 3:
 	//find user
